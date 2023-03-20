502f673f909fa1e1210926dec33faf719da15468
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-03-20 23:47:18 +0530

    Update: box.jl

--- box.jl
+++ box.jl
@@ -3,12 +3,35 @@ using Gtk
 win = GtkWindow("Box Code")
 hbox = GtkBox(:h) # :h makes the layout horizontal
 
-b = GtkButton("Click Me")
-push!(win,b)
+push!(win, hbox)
+cancel = GtkButton("Cancel")
 
-function on_button_clicked(w)
-  println("The button has been clicked")
+ok = GtkButton("Ok")
+push!(hbox, cancel)
+push!(hbox, ok)
+
+set_gtk_property!(hbox, :expand, true)
+set_gtk_property!(hbox, :spacing, 10)
+
+# better layout starts here
+destroy(hbox)
+
+ok = GtkButton("Ok")
+cancel = GtkButton("Cancel")
+
+hbox = GtkButtonBox(:h)
+push!(win, hbox)
+push!(hbox, cancel)
+push!(hbox, ok)
+
+function on_click(w)
+  println("Button was clicked")
 end
-signal_connect(on_button_clicked, b, "clicked")
+signal_connect(on_click, ok, "clicked")
+
+function on_cancel(w)
+  println("Cancel button was clicked")
+end
+signal_connect(on_cancel, cancel, "clicked") # Over here clicked is a signal emitted when user clicks a button 
 
 showall(win)
